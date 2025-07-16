c74699c4c8f15f12bd1a41df2acf9d93464ef9e5
Yevhen412 <neogen123456789@gmail.com>
2025-07-16 21:26:25 +0200

    Update telegram_notifier.py

--- telegram_notifier.py
+++ telegram_notifier.py
@@ -4,7 +4,7 @@ import aiohttp
 BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "").strip()
 CHAT_ID = os.getenv("TELEGRAM_CHAT_ID", "").strip()
 
-async def send_telegram_message(text: str):
+async def send_telegram_message(message: str):
     if not BOT_TOKEN or not CHAT_ID:
         print("❌ Переменные окружения не заданы!")
         return
@@ -12,7 +12,7 @@ async def send_telegram_message(text: str):
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
     payload = {
         "chat_id": CHAT_ID,
-        "text": text,
+        "text": message,
         "parse_mode": "HTML"
     }
 
