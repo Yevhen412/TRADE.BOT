76bbadc463efaffc50e867b05fddf70e33eec432
Yevhen412 <neogen123456789@gmail.com>
2025-07-18 10:07:16 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,22 +1,9 @@
 import asyncio
-from fastapi import FastAPI
-from telegram_notifier import send_telegram_message
 from websocket_client import run_session
 
-app = FastAPI()
+def run():
+    print("🚀 Сессия запускается...")
+    asyncio.run(run_session())
 
-@app.get("/start")
-async def handle_start():
-    # 🔄 Запуск стратегии на 2 минуты
-    asyncio.create_task(run_session())
-
-    # Заменяем переменную на явный домен
-    restart_link = "https://tradebot-production-c405.up.railway.app/start"
-
-    # 📬 Уведомление в Telegram
-    await send_telegram_message(
-        f"✅ <b>Процесс запущен на 2 минуты.</b>\n"
-        f"🔁 Для нового запуска: <a href=\"{restart_link}\">{restart_link}</a>"
-    )
-
-    return {"message": "Strategy started"}
+if __name__ == "__main__":
+    run()
