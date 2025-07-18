a37fbf7a810555554796c00fc4a10d2ee5fa1a37
Yevhen412 <neogen123456789@gmail.com>
2025-07-18 07:21:24 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+import asyncio
+from websocket_client import connect_websocket
 
 app = FastAPI()
 
+@app.on_event("startup")
+async def startup_event():
+    asyncio.create_task(connect_websocket())
+
 @app.get("/start")
 async def start():
-    return {"message": "Asyncio работает"}
+    return {"message": "Все работает"}
