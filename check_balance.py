d8fb434f45c2ae59df044e99912c847e2ad6a39e
Yevhen412 <neogen123456789@gmail.com>
2025-07-19 20:59:42 +0200

    Update check_balance.py

--- check_balance.py
+++ check_balance.py
@@ -1,23 +1,12 @@
-from bybit import UnifiedMargin
+from pybit.unified_trading import HTTP
 import os
 from dotenv import load_dotenv
 
-# Загружаем переменные окружения
 load_dotenv()
-
-api_key = os.getenv("BYBIT_API_KEY")
-api_secret = os.getenv("BYBIT_API_SECRET")
-
-# Инициализируем соединение
-client = UnifiedMargin(
-    api_key=api_key,
-    api_secret=api_secret,
-    testnet=False  # True — если используешь testnet
+session = HTTP(
+    testnet=False,
+    api_key=os.getenv("API_KEY"),
+    api_secret=os.getenv("API_SECRET")
 )
-
-# Получаем баланс
-response = client.get_wallet_balance(accountType="UNIFIED")
-
-# Показываем результат
-print("🔥 Баланс:")
-print(response)
+response = session.get_wallet_balance(accountType="UNIFIED")
+print("🔥 Баланс:", response)
